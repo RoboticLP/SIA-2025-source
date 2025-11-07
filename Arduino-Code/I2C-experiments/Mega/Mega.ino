7b9f19734cea7552fb894343993d54926df58055
RoboticLP <106448941+RoboticLP@users.noreply.github.com>
2025-11-07 16:13:22 +0100

    Added first project files for I2C communication (only reqeusts)
    Introduces Mega.ino as an I2C master requesting data from two slave devices, and Slave.ino as an I2C slave responding with a message. These examples demonstrate basic I2C communication between Arduino boards.

---
+++ Arduino-Code/I2C-experiments/Mega/Mega.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+
+  Serial.begin(9600);  // start serial for output
+}
+
+void loop() {
+  Wire.requestFrom(6, 15);    // request 6 bytes from peripheral device #6 (arduino uno)
+  while (Wire.available()) { // peripheral may send less than requested
+    char c = Wire.read(); // receive a byte as character
+
+    Serial.print(c);         // print the character
+  }
+  Serial.print("\n");
+
+  Wire.requestFrom(2, 18);    // request 6 bytes from peripheral device #2 (arduino nano)
+  while (Wire.available()) { // peripheral may send less than requested
+    char c = Wire.read(); // receive a byte as character
+
+    Serial.print(c);         // print the character
+  }
+  Serial.print("\n");
+
+  delay(500);
+}
