a65bf8c824f41709d4fa6c14f1048844640ffeaa
TheRedEnd2000 <fkircher2007@gmail.com>
2025-11-14 16:10:12 +0100

    versuchen erste signale zu lesen

--- Arduino-Code/I2C-experiments/Slave/Slave_2/Slave_2.ino
+++ Arduino-Code/I2C-experiments/Slave/Slave_2/Slave_2.ino
@@ -1,33 +1,45 @@
 #include <Wire.h>
 
-int sensor1 = 1;
+int sensor1 = A0;
 int sensor2 = 2;
 int sensor3 = 3;
 int sensor4 = 4;
 
-int pointsToAdd = 0;
+int hits_goals = 0;
 
 char message[50];
 
 void setup() {  
   Serial.begin(9600);
+  pinMode(sensor1, INPUT);
+
   Wire.begin(2);  // Arduino als I2C-Slave mit Adresse 2
+
   Wire.onRequest(requestEvent);  // registriere den Event für Datenanforderungen
+  //attachInterrupt(digitalPinToInterrupt(sensor1),hit_Goal,RISING);
 }
 
 void loop() {
-  check();
-  delay(1000);  // 1 Sekunde warten
-  pointsToAdd += random(1, 10);  // Zufällige Punkte zwischen 1 und 10 hinzufügen
+  if(digitalRead(sensor1) == 1) hit_Goal();
+  delay(200);
 }
 
-void check() {
-  Serial.println("Points to add: " + String(pointsToAdd));  // String statt Verkettung
-  pointsToAdd = 0;  // Punkte zurücksetzen
+void hit_Goal(){
+  hits_goals++;
+  Serial.println("Hit registered Now: "+hits_goals);
+}
+
+void sendHits_GoalsData(){
+  sprintf(message, "ht1:%d|",hits_goals);
+  hits_goals = 0;
+}
+
+void sendData(){
+  sendHits_GoalsData();
+  Wire.write(message);
 }
 
 void requestEvent() {
-  sprintf(message, "Points to add: %d", pointsToAdd);
-  Wire.write(message);  // Nachricht an den Master senden
-  pointsToAdd = 0;  // Punkte nach dem Senden zurücksetzen
+  sendData();
+  Serial.println("Daten gesendet");
 }
