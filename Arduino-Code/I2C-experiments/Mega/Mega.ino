4b64b02ea607fd49fe0d8b5f9dfd8afc8e09b2af
RoboticLP <adrimic@web.de>
2025-11-18 21:17:28 +0100

    Refactor splitString utility to separate header file
    Moved the splitString function from Mega.ino to a new utils.h header file for better code organization and reusability. Updated Mega.ino to include utils.h and use the function from the header.

--- Arduino-Code/I2C-experiments/Mega/Mega.ino
+++ Arduino-Code/I2C-experiments/Mega/Mega.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include "utils.h"
 
 #define slave2 2
 #define slave3 3
@@ -31,7 +32,7 @@ void loop() {
 
     // process data recieved by that slave
     int dataCount;
-    String* data = splitString(test, '|', dataCount);
+    String* data = splitString(answer, '|', dataCount);
     for (int j = 0; j < dataCount; j++) {
       if (data[j].indexOf(':') != -1) { // process substring if its a key with a value (ex.: ht1:1)
         int count;
@@ -57,46 +58,4 @@ void loop() {
 
   // request admin panel less often with same logic as above
   delay(2000);
-}
-
-/* Utility functions */
-
-/* since Arduino doesn't have a builtin way to split Strings, this exists
-Usage:
-int dataCount;
-String* segments = splitString(inputString, '|', dataCount);
-for (int j = 0; j < dataCount; j++) {
-  segments[j]; // to access substrings
-  // process substrings here
-}
-delete[] segments; // delete array to prevent memory leaks
-*/
-String* splitString(String input, char splitter, int &count) {
-  // Count how many splits we'll have
-  count = 1;
-  for (int i = 0; i < input.length(); i++) {
-    if (input.charAt(i) == splitter) {
-      count++;
-    }
-  }
-  
-  // Create array to hold the substrings
-  String* result = new String[count];
-  
-  // Split the string
-  int index = 0;
-  int lastPos = 0;
-  
-  for (int i = 0; i < input.length(); i++) {
-    if (input.charAt(i) == splitter) {
-      result[index] = input.substring(lastPos, i);
-      index++;
-      lastPos = i + 1;
-    }
-  }
-  
-  // Add the last substring
-  result[index] = input.substring(lastPos);
-  
-  return result;
 }
