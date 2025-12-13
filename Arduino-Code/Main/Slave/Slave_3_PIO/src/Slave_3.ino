30eb23ca5921430c863bf5bc5acc8168e5883395
Gamermundi <241511232+Gamermundi@users.noreply.github.com>
2025-12-13 16:04:09 +0100

    Implement method for triggering bumpers, updated requestEvent method to give the correct data format to the master

--- Arduino-Code/Main/Slave/Slave_3_PIO/src/Slave_3.ino
+++ Arduino-Code/Main/Slave/Slave_3_PIO/src/Slave_3.ino
@@ -6,10 +6,23 @@
 #define interrupt_1 2
 #define interrupt_2 3
 
+int numberOfBumpers = 3;
+int bumper[3] = {Spule_1,Spule_2,Spule_3};
+
+int bumperHits = 0;
+
+String error_module3 = "";
+
+char requestEventAnswer[50]; //text thats 50 bytes long to send the master module
+
 void setup() {  
   Wire.begin(3);                // join i2c bus with address #8
 
   Wire.onRequest(requestEvent); // register event
+
+  for(int i = 0; i < numberOfBumpers; i++){ //configure the pins that control the coils
+    pinMode(bumper[i],OUTPUT);
+  }
 }
 
 void loop() {
@@ -17,12 +30,23 @@ void loop() {
   
 }
 
+//method to trigger a bumper, executed by the interrupt pins in  void loop
+void triggerBumper(int number){
+  digitalWrite(bumper[number],HIGH);
+  bumperHits ++;
+}
+
 // function that executes whenever data is requested by master
 
 // this function is registered as an event, see setup()
 
 void requestEvent() {
+  int ht1Dummy = bumperHits;
+  snprintf(requestEventAnswer,sizeof(requestEventAnswer),"ht1:%d|ht2:-1|err:%s",ht1Dummy,error_module3);
 
-  Wire.write("mac upload"); // respond with message of 6 bytes
+  Wire.write(requestEventAnswer); // respond with message of 6 bytes
+
+  bumperHits -= ht1Dummy;
+  ht1Dummy == 0;
   // as expected by master
 }
