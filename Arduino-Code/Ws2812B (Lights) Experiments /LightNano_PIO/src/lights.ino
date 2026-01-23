4f80f1eaee542cc078bccbb2c07efc5c64b4aaa2
Gamermundi <241511232+Gamermundi@users.noreply.github.com>
2026-01-23 16:32:11 +0100

    first steps for the light code

--- Arduino-Code/Ws2812B (Lights) Experiments /LightNano_PIO/src/lights.ino
+++ Arduino-Code/Ws2812B (Lights) Experiments /LightNano_PIO/src/lights.ino
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-#define DATAPIN 6;
-#define NUMPIXELS 20;
+#define PIN 6
+#define NUMPIXELS 20
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, DATAPIN, NEO_GRB, NEO_KHZ800)
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   pixels.begin();
