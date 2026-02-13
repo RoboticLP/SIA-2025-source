8d34512566f0dc66977165b6d4e86ff11a3e7792
FlussWolke751 <marius.goelz@t-online.de>
2026-02-13 16:17:15 +0100

    Slave2 Version4.1

--- Arduino-Code/Main/Slave/Slave_2_PIO/src/Slave_2.ino
+++ Arduino-Code/Main/Slave/Slave_2_PIO/src/Slave_2.ino
@@ -6,7 +6,7 @@ int SlingshotReader = 0; //Auswertung
 int scoredTimes = 0; //Auswertung
 
 unsigned long TasterZeit = 0; // Zeit beim drücken ders Tasters
-int TasterGedrückt = 0; // If-Clause Bestätigung
+int TasterGedrueckt = 0; // If-Clause Bestätigung
 
 int entprellZeit = 200; // 200 ms
 
@@ -26,22 +26,21 @@ void setup() {
 void loop() {
 
 
- if(digitalRead(scoring) == LOW) {
- TasterZeit = millis();                // aktualisiert die Tasterzeit
- TasterGedrückt = TasterGedrückt + 1;  // registriert das Signal und speichert es als Variable
- }
- if(millis() - TasterZeit > entprellZeit && TasterGedrückt == 1) {
- TasterGedrückt = 0;                   // zurücksetzen 
- scoredTimes = scoredTimes + 1;
- }
+if(digitalRead(scoring) == LOW) {
+TasterZeit = millis();                // aktualisiert die Tasterzeit
+TasterGedrueckt = TasterGedrueckt + 1;  // registriert das Signal und speichert es als Variable
+}
+if(millis() - TasterZeit > entprellZeit && TasterGedrueckt == 1) {
+TasterGedrueckt = 0;                   // zurücksetzen 
+scoredTimes = scoredTimes + 1;
+}
 
- if(scoredTimes > SlingshotReader) {
+if(scoredTimes > SlingshotReader) {
     Serial.println ("Slingshots wurden ausgelöst");
     Serial.println(scoredTimes);
     // code...
     SlingshotReader = scoredTimes;
-  }
- }
+ } }
 //___________________________void requestEvent_______________________
 void requestEvent() {
   sprintf(message, "ssh:%d|", scoredTimes);
